754631f308e4e889e370246135882e21840b319e
possap-gov-ng-validate-document <veralanxo@gmail.com>
2025-12-16 11:10:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Page Navigation Functions
 function showDocumentInfo() {
     // Navigate to the document info page
-    window.location.href = 'validate-document/PSS08180000755370';
+    window.location.href = 'validate-document/PSS08180000755390';
 }
 
 function showValidationForm() {
